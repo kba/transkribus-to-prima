992075b2ea1add21aab6be2988bec9a5da050261
Konstantin Baierer <unixprog@gmail.com>
2021-08-26 15:19:13 +0200

    make as namespace-agnostic as possible

--- transkribus_fixer/fixer.py
+++ transkribus_fixer/fixer.py
@@ -13,18 +13,21 @@ class TranskribusFixer():
         self.tree = tree
 
     def fix_metadata(self):
-        el_metadata = self.tree.find('//p2013:TranskribusMetadata', NS)
-        if el_metadata is not None:
-            el_metadata.getparent().remove(el_metadata)
+        el_metadata = self.tree.xpath('//*[local-name()="TranskribusMetadata"]')
+        if el_metadata:
+            el_metadata[0].getparent().remove(el_metadata[0])
 
     def fix_reading_order(self):
-        ro = self.tree.find('//p2013:ReadingOrder/p2013:OrderedGroup', NS)
-        relations = self.tree.find('//p2013:Relations', NS)
-        for relation in relations.findall('p2013:Relation[@type="link"]', NS):
-            region_refs = relation.findall('p2013:RegionRef', NS)
+        ro = self.tree.xpath('//*[local-name()="ReadingOrder"]/*[local-name()="OrderedGroup"]')[0]
+        relations = self.tree.xpath('//*[local-name()="Relations"]')
+        if not relations:
+            return
+        relations = relations[0]
+        for relation in relations.xpath('*[local-name()="Relation"][@type="link"]'):
+            region_refs = relation.xpath('*[local-name()="RegionRef"]')
             ro_grp = ET.SubElement(ro, 'OrderedGroupIndexed')
             ro_grp.set('id', 'relation_link_' + '-'.join([x.get('regionRef') for x in region_refs]))
-            ro_grp.set('index', str(int(ro.findall('p2013:RegionRefIndexed', NS)[-1].get('index')) + 1))
+            ro_grp.set('index', str(int(ro.xpath('*[local-name()="RegionRefIndexed"]')[-1].get('index')) + 1))
             for idx, region_ref in enumerate(region_refs):
                 ro_region_ref = ET.SubElement(ro_grp, 'RegionRefIndexed')
                 ro_region_ref.set('regionRef', region_ref.get('regionRef'))
@@ -34,21 +37,21 @@ class TranskribusFixer():
             relations.getparent().remove(relations)
 
     def fix_table(self):
-        for el_table in self.tree.findall('//p2013:TableRegion', NS):
-            for el_cell in el_table.findall('p2013:TableCell', NS):
+        for el_table in self.tree.xpath('//*[local-name()="TableRegion"]'):
+            for el_cell in el_table.xpath('*[local-name()="TableCell"]'):
                 el_table.remove(el_cell)
                 el_region = ET.SubElement(el_table, '{%s}TextRegion' % NS2013)
-                el_region.insert(0, el_cell.find('p2013:Coords', NS))
+                el_region.insert(0, el_cell.xpath('*[local-name()="Coords"]')[0])
                 el_roles = ET.SubElement(el_region, '{%s}Roles' % NS2013)
                 el_tablecellrole = ET.SubElement(el_roles, '{%s}TableCellRole' % NS2013)
                 el_tablecellrole.set('rowIndex', el_cell.get('row'))
                 el_tablecellrole.set('columnIndex', el_cell.get('col'))
-                el_tablecellrole.set('rowSpan', el_cell.get('rowSpan'))
-                el_tablecellrole.set('colSpan', el_cell.get('colSpan'))
+                el_tablecellrole.set('rowSpan', el_cell.get('rowSpan', '1'))
+                el_tablecellrole.set('colSpan', el_cell.get('colSpan', '1'))
                 for att in ['orientation', 'id']:
                     if att in el_cell.attrib:
                         el_region.set(att, el_cell.get(att))
-                for el_textline in el_cell.findall('p2013:TextLine', NS):
+                for el_textline in el_cell.xpath('*[local-name()="TextLine"]'):
                     el_region.append(el_textline)
 
     def tostring(self):
