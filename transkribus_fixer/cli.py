40bc502c6e1696614edfea1cde0231b25725fff8
Robert Sachunsky <sachunsky@informatik.uni-leipzig.de>
2021-12-15 13:50:44 +0100

    CLI: delegate to click.File

--- transkribus_fixer/cli.py
+++ transkribus_fixer/cli.py
@@ -1,26 +1,25 @@
-from click import command, Choice, argument, option
+from click import command, Choice, File, argument, option
 from lxml import etree as ET
 
 from .fixer import TranskribusFixer, NS
 
-@command()
+@command(context_settings={'help_option_names': ['-h', '--help']})
 @option('-f', '--fixes', help="Fixes to apply. Repeatable", type=Choice(['reading_order', 'table', 'metadata', 'namespace']), multiple=True)
-@argument('input-file', nargs=1)
-@argument('output-file', nargs=1)
+@argument('input-file', type=File('r'), nargs=1)
+@argument('output-file', default='-', type=File('w'), nargs=1)
 def cli(fixes, input_file, output_file):
     """
-    Apply the fixes to INPUT_FILE.
+    Transform (Transkribus PAGE) INPUT_FILE to (PRImA PAGE) OUTPUT_FILE under the chosen fixes.
 
-    Also converts PAGE2013 to PAGE2019 when writing to OUTPUT_FILE.
+    Also convert PAGE namespace version from 2013 to 2019.
     """
     fixer = TranskribusFixer(ET.parse(input_file))
     for fix in [f for f in fixes if f != 'namespace']:
         getattr(fixer, f'fix_{fix}')()
-    with open('/dev/stdout' if output_file == '-' else output_file, 'w') as f:
-        as_str = fixer.tostring()
-        if 'namespace' in fixes:
-            as_str = as_str.replace(NS['p2013'], NS['p2019'])
-        f.write(as_str)
+    as_str = fixer.tostring()
+    if 'namespace' in fixes:
+        as_str = as_str.replace(NS['p2013'], NS['p2019'])
+    output_file.write(as_str)
 
 if __name__ == "__main__":
     cli()
