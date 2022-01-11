1bf0e34ca67c3c94b198cfc5fb0803908bcea6ff
Robert Sachunsky <38561704+bertsky@users.noreply.github.com>
2022-01-11 13:52:45 +0100

    Fixes #1, #2, #3, #4, #6 and #8.
    CLI: delegate to click.File
    CLI: default to all fixers
    TableCell: convert @Label, too
    TableCell/Region: preserve all std attributes and elements
    add option for validation of output
    fix TextEquiv/UnicodeAlternative
    delegate to TranskribusFixer class for choices available, add docstrings
    add fixer for Page/@image* transformation attributes
    add fixer removing Tag, Property and Link elements
    add option to promote TranskribusMetadata/@imgurl to @imageFilename

--- transkribus_fixer/fixer.py
+++ transkribus_fixer/fixer.py
@@ -9,18 +9,26 @@ class TranskribusFixer():
     Translates Transkribus variant of PAGE to standard-conformant PAGE
     """
 
-    def __init__(self, tree):
+    def __init__(self, tree, prefer_imgurl=False):
         self.tree = tree
+        self.prefer_imgurl = prefer_imgurl
 
     def fix_metadata(self):
-        el_metadata = self.tree.xpath('//*[local-name()="TranskribusMetadata"]')
-        if el_metadata:
-            el_metadata[0].getparent().remove(el_metadata[0])
+        """Remove any Metadata/TranskribusMetadata"""
+        el_page = self.tree.find('{*}Page')
+        el_metadata = self.tree.find('{*}Metadata')
+        if el_metadata is not None:
+            el_metadata = el_metadata.find('{*}TranskribusMetadata')
+        if el_metadata is not None:
+            if self.prefer_imgurl and 'imgUrl' in el_metadata.attrib:
+                el_page.attrib['imageFilename'] = el_metadata.attrib['imgUrl']
+            el_metadata.getparent().remove(el_metadata)
 
     def fix_reading_order(self):
+        """Convert ???"""
         ro = self.tree.xpath('//*[local-name()="ReadingOrder"]/*[local-name()="OrderedGroup"]')[0]
         relations = self.tree.xpath('//*[local-name()="Relations"]')
-        if not relations:
+        if not len(relations):
             return
         relations = relations[0]
         for relation in relations.xpath('*[local-name()="Relation"][@type="link"]'):
@@ -37,22 +45,92 @@ class TranskribusFixer():
             relations.getparent().remove(relations)
 
     def fix_table(self):
+        """Convert each TableRegion/TableCell into a TableRegion/TextRegion, writing row/col index/span as new TableCellRole accordingly"""
         for el_table in self.tree.xpath('//*[local-name()="TableRegion"]'):
             for el_cell in el_table.xpath('*[local-name()="TableCell"]'):
                 el_table.remove(el_cell)
                 el_region = ET.SubElement(el_table, '{%s}TextRegion' % NS2013)
-                el_region.insert(0, el_cell.xpath('*[local-name()="Coords"]')[0])
-                el_roles = ET.SubElement(el_region, '{%s}Roles' % NS2013)
+                for att in ['id', 'custom', 'comments', 'continuation', 'production',
+                            'orientation', 'type', 'leading', 'indented', 'align',
+                            'readingDirection', 'readingOrientation', 'textLineOrder',
+                            'primaryLanguage', ',secondaryLanguage',
+                            'primaryScript', 'secondaryScript']:
+                    if att in el_cell.attrib:
+                        el_region.set(att, el_cell.get(att))
+                for node in el_cell.iterchildren("{*}AlternativeImage", "{*}Coords",
+                                                 "{*}UserDefined", "{*}Labels",
+                                                 "{*}Roles"):
+                    el_region.append(node)
+                el_roles = el_region.find('{*}Roles')
+                if el_roles is None:
+                    el_roles = ET.SubElement(el_region, '{%s}Roles' % NS2013)
+                # NS2013 does not have TableCellRole, so we implicitly rely on the namespace fixer here
                 el_tablecellrole = ET.SubElement(el_roles, '{%s}TableCellRole' % NS2013)
                 el_tablecellrole.set('rowIndex', el_cell.get('row'))
                 el_tablecellrole.set('columnIndex', el_cell.get('col'))
                 el_tablecellrole.set('rowSpan', el_cell.get('rowSpan', '1'))
                 el_tablecellrole.set('colSpan', el_cell.get('colSpan', '1'))
-                for att in ['orientation', 'id']:
-                    if att in el_cell.attrib:
-                        el_region.set(att, el_cell.get(att))
-                for el_textline in el_cell.xpath('*[local-name()="TextLine"]'):
-                    el_region.append(el_textline)
+                el_tablecellrole.set('header', el_cell.get('label', "false"))
+                for node in el_cell.iterchildren():
+                    if any(node.tag.endswith(suf)
+                           for suf in ['Region', 'TextLine', 'TextEquiv', 'TextStyle']):
+                        el_region.append(node)
+
+    def fix_textequiv(self):
+        """Convert any //TextEquiv/UnicodeAlternative into additional ../TextEquiv/Unicode"""
+        for el_te in self.tree.xpath('//*[local-name()="TextEquiv"]'):
+            for el_uni in el_te.xpath('//*[local-name()="UnicodeAlternative"]'):
+                el_te.remove(el_uni)
+                el_tenew = ET.SubElement(el_te.getparent(), '{%s}TextEquiv' % NS2013)
+                el_tenewuni = ET.SubElement(el_tenew, '{%s}Unicode' % NS2013)
+                el_tenewuni.text = el_uni.text
+
+    def fix_image_transform(self):
+        """Convert Page/@image(Rotation|Translation|Scaling) to Labels"""
+        el_page = self.tree.find('{*}Page')
+        el_labels = el_page.find('{*}Labels')
+        def new_label(typ, val):
+            nonlocal el_labels
+            if el_labels is None:
+                el_labels = ET.Element('{%s}Labels' % NS2013)
+                el_labels.set('externalModel', 'TranskribusImageTransform')
+                # find position to insert (labels go right before regions)
+                regions = el_page.xpath('*[contains(local-name(),"Region")]')
+                if len(regions):
+                    regions[0].addprevious(el_labels)
+                else:
+                    el_page.append(el_labels)
+            el_label = ET.SubElement(el_labels, '{%s}Label' % NS2013)
+            el_label.set('type', typ)
+            el_label.set('value', val)
+        for label in ['imageRotation',
+                      'imageTranslationX',
+                      'imageTranslationY',
+                      'imageScalingX',
+                      'imageScalingY']:
+            if label in el_page.attrib:
+                new_label(label, el_page.attrib.pop(label))
+
+    def fix_tag_property_link(self):
+        """Remove Tag, Property and Link elements whereever they appear"""
+        # all known under PageType, RegionType, TextLineType, WordType, GlyphType
+        # Tag known under TextEquivType
+        # Property known under TranskribusMetadataType
+        # ...but we might as well search everywhere
+        # FIXME: convert these to Labels and Relations
+        for node in self.tree.iter():
+            tag = node.find('{*}Tag')
+            if tag is not None:
+                node.remove(tag)
+            prop = node.find('{*}Property')
+            if prop is not None:
+                node.remove(prop)
+            link = node.find('{*}Link')
+            if link is not None:
+                node.remove(link)
 
     def tostring(self):
-        return ET.tostring(self.tree, pretty_print=True, encoding='utf-8').decode('utf-8')
+        return ET.tostring(self.tree,
+                           pretty_print=True,
+                           xml_declaration=True,
+                           encoding='utf-8').decode('utf-8')
